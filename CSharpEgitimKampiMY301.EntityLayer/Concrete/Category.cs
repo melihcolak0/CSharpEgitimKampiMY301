02ea103320f6d4e8623feea1d0dbe305ef26d2fd
Melih Çolak <melihcolak0@gmail.com>
2024-10-29 16:27:20 +0300

    Add project files.

---
+++ CSharpEgitimKampiMY301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampiMY301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        // Field - Variable - Property
+        /*
+        int x; // Field
+
+        public int y { get; set; } // Property
+
+        void test()
+        {
+            int z; // Variable
+        }
+        */
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
