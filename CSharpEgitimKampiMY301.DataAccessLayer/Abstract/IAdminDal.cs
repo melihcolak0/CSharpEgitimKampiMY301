0177c57e8dd8ad5acd44fb5c79a17c883e77d423
Melih Çolak <melihcolak0@gmail.com>
2024-11-09 16:28:22 +0300

    ...

---
+++ CSharpEgitimKampiMY301.DataAccessLayer/Abstract/IAdminDal.cs
@@ -0,0 +1,14 @@
+﻿using CSharpEgitimKampiMY301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampiMY301.DataAccessLayer.Abstract
+{
+    public interface IAdminDal : IGenericDal<Admin>
+    {
+
+    }
+}
