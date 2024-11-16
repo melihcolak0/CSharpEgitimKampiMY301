7ab04ff2b53d4e809eb9262fcc1ac590f698e352
Melih Çolak <melihcolak0@gmail.com>
2024-11-16 20:05:00 +0300

    ...

--- CSharpEgitimKampiMY301.EFProject/Form1.cs
+++ CSharpEgitimKampiMY301.EFProject/Form1.cs
@@ -16,5 +16,49 @@ namespace CSharpEgitimKampiMY301.EFProject
         {
             InitializeComponent();
         }
+
+        CSharpEgitimKampi301EFTravelDbEntities db = new CSharpEgitimKampi301EFTravelDbEntities();
+
+        private void btnList_Click(object sender, EventArgs e)
+        {            
+            var values = db.TblGuide.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            TblGuide tblGuide = new TblGuide();
+            tblGuide.GuideName = txtName.Text;
+            tblGuide.GuideSurname = txtSurname.Text;
+            db.TblGuide.Add(tblGuide);
+            db.SaveChanges();
+            MessageBox.Show("Rehber başarıyla eklendi.");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var removeValue = db.TblGuide.Find(id);
+            db.TblGuide.Remove(removeValue);
+            db.SaveChanges();
+            MessageBox.Show("Rehber başarıyla silindi.");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var updateValue = db.TblGuide.Find(id);
+            updateValue.GuideName = txtName.Text;
+            updateValue.GuideSurname = txtSurname.Text;
+            db.SaveChanges();
+            MessageBox.Show("Rehber başarıyla güncellendi.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void btnGetById_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var values = db.TblGuide.Where(x => x.GuideId == id).ToList();
+            dataGridView1.DataSource = values;
+        }
     }
 }
