b5eb9a506cfe1d6c585e4ca6f8bb9ee40c1af1c7
Melih Çolak <melihcolak0@gmail.com>
2024-11-19 13:11:50 +0300

    ...

---
+++ CSharpEgitimKampiMY301.EFProject/FrmLocation.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampiMY301.EFProject
+{
+    public partial class FrmLocation : Form
+    {
+        public FrmLocation()
+        {
+            InitializeComponent();
+        }
+
+        CSharpEgitimKampi301EFTravelDbEntities db = new CSharpEgitimKampi301EFTravelDbEntities();
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = db.TblLocation.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void FrmLocation_Load(object sender, EventArgs e)
+        {
+            var values = db.TblGuide.Select(x => new
+            {
+                FullName = x.GuideName + " " + x.GuideSurname,
+                x.GuideId
+            }).ToList();
+
+            cmbGuide.DisplayMember = "FullName";
+            cmbGuide.ValueMember = "GuideId";
+            cmbGuide.DataSource = values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            TblLocation location = new TblLocation();
+            location.Capacity = byte.Parse(nudCapacity.Value.ToString()); // tinyint in SQL Server
+            location.City = txtCity.Text;
+            location.Country = txtCountry.Text;
+            location.Price = decimal.Parse(txtPrice.Text);
+            location.DayNight = txtDayNight.Text;
+            location.GuideId = int.Parse(cmbGuide.SelectedValue.ToString());
+
+            db.TblLocation.Add(location);
+            db.SaveChanges();
+            MessageBox.Show("Ekleme işlemi başarılı.");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var deletedValue = db.TblLocation.Find(id);
+            db.TblLocation.Remove(deletedValue);
+            db.SaveChanges();
+            MessageBox.Show("Silme işlemi başarılı.");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var updatedValue = db.TblLocation.Find(id);
+            updatedValue.Capacity = byte.Parse(nudCapacity.Value.ToString()); // tinyint in SQL Server
+            updatedValue.City = txtCity.Text;
+            updatedValue.Country = txtCountry.Text;
+            updatedValue.Price = decimal.Parse(txtPrice.Text);
+            updatedValue.DayNight = txtDayNight.Text;
+            updatedValue.GuideId = int.Parse(cmbGuide.SelectedValue.ToString());
+
+            db.SaveChanges();
+            MessageBox.Show("Güncelleme işlemi başarılı.");
+        }
+    }
+}
