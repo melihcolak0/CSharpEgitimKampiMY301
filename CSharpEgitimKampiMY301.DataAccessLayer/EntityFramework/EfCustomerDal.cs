7dded8b4778e770f5ad98175fdb7b22ff9557bc4
Melih Çolak <melihcolak0@gmail.com>
2024-11-26 16:59:23 +0300

    ...

---
+++ CSharpEgitimKampiMY301.DataAccessLayer/EntityFramework/EfCustomerDal.cs
@@ -0,0 +1,15 @@
+﻿using CSharpEgitimKampiMY301.DataAccessLayer.Abstract;
+using CSharpEgitimKampiMY301.DataAccessLayer.Repositories;
+using CSharpEgitimKampiMY301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampiMY301.DataAccessLayer.EntityFramework
+{
+    public class EfCustomerDal : GenericRepository<Customer>, ICustomerDal
+    {
+    }
+}
