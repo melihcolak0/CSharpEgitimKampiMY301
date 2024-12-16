2df38f583efeabd23ce0904e7c4b5708351192dc
Melih Çolak <melihcolak0@gmail.com>
2024-12-16 20:59:19 +0300

    ...

--- CSharpEgitimKampiMY301.PresentationLayer/FrmProduct.cs
+++ CSharpEgitimKampiMY301.PresentationLayer/FrmProduct.cs
@@ -1,6 +1,7 @@
 ﻿using CSharpEgitimKampiMY301.BusinessLayer.Abstract;
 using CSharpEgitimKampiMY301.BusinessLayer.Concrete;
 using CSharpEgitimKampiMY301.DataAccessLayer.EntityFramework;
+using CSharpEgitimKampiMY301.EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -16,10 +17,12 @@ namespace CSharpEgitimKampiMY301.PresentationLayer
     public partial class FrmProduct : Form
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
         public FrmProduct()
         {
             InitializeComponent();
             _productService = new ProductManager(new EfProductDal());
+            _categoryService = new CategoryManager(new EfCategoryDal());
         }       
 
         private void btnList_Click(object sender, EventArgs e)
@@ -34,9 +37,55 @@ namespace CSharpEgitimKampiMY301.PresentationLayer
             dataGridView1.DataSource = values;
         }
 
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Product product = new Product();
+            product.CategoryId = int.Parse(cmbCategory.SelectedValue.ToString());
+            product.ProductPrice = decimal.Parse(txtProductPrice.Text);
+            product.ProductName = txtProductName.Text;
+            product.ProductDescription = txtDescription.Text;
+            product.ProductStock = int.Parse(txtProductStock.Text);
+
+            _productService.TInsert(product);
+            MessageBox.Show("Ekleme başarılı!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+        }
+
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            
+            int id = int.Parse(txtProductId.Text);
+            var value = _productService.TGetById(id);
+            _productService.TDelete(value);
+            MessageBox.Show("Silme başarılı!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtProductId.Text);
+            var value = _productService.TGetById(id);
+            value.CategoryId = int.Parse(cmbCategory.SelectedValue.ToString());
+            value.ProductDescription = txtDescription.Text;
+            value.ProductPrice = decimal.Parse(txtProductPrice.Text);
+            value.ProductStock = int.Parse(txtProductStock.Text);
+            value.ProductName = txtProductName.Text;
+
+            _productService.TUpdate(value);
+            MessageBox.Show("Güncelleme başarılı!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+        }
+
+        private void btnGetById_Click(object sender, EventArgs e)
+        {            
+            int id = int.Parse(txtProductId.Text);
+            var value = _productService.TGetById(id);
+            dataGridView1.DataSource = new List<Product> { value };
+        }
+
+        private void FrmProduct_Load(object sender, EventArgs e)
+        {
+            var values = _categoryService.TGetAll();
+            cmbCategory.DataSource = values;
+            cmbCategory.DisplayMember = "CategoryName";
+            cmbCategory.ValueMember = "CategoryId";
         }
     }
 }
